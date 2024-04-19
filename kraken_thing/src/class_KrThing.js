ee3a5a7ef1025cb8fd44adce9357221af1f545fe
tactik8 <48621231+tactik8@users.noreply.github.com>
2024-04-19 17:38:53 +0000

    go

--- kraken_thing/src/class_KrThing.js
+++ kraken_thing/src/class_KrThing.js
@@ -1,5 +1,8 @@
 
-import { KrThing as KrThingRecord } from '../kraken_record/kraken_record.js';
+//import { KrThing as KrThingRecord } from '../kraken_record/kraken_record.js';
+import { KrThing as KrThingRecord } from 'https://cdn.jsdelivr.net/gh/tactik8/krakenJsLibraryRecord@main/kraken_thing/kraken_thing.js';
+
+
 import { KrakenSchemas } from '../kraken_schema/kraken_schema.js';
 import { KrSamples } from '../kraken_schema/kraken_schema.js';
 
